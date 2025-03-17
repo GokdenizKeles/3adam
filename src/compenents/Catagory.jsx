678f766f610a7006ed87c5c650270b7c13441f1a
[Atakan Arıkan] <[atakanarkn1@outlook.com]>
2025-03-17 19:48:56 +0300

    main page tamamlandı

--- src/compenents/Catagory.jsx
+++ src/compenents/Catagory.jsx
@@ -6,13 +6,13 @@ export default function Category() {
 
   return(
     <div className="category-area">
-      <div className="category-header">
+      {/* <div className="category-header">
         <button><img src="./img/back-icon.svg"/></button>
         <h6>Pick a Category</h6>
         <div className="categories">
           <h4>{data.Category}</h4>
         </div>
-      </div>
+      </div> */}
     </div>
   )
 }
